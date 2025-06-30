da8193efb77c7f3fe6f869d38eea803ef600d5be
mamadousy92i <92mamadousy@gmail.com>
2025-06-30 22:38:56 +0000

    model et serializer nice

--- backend/money_transfer/money_transfer/settings.py
+++ backend/money_transfer/money_transfer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='&@dsj23!1-x-38f))ld&b%d!#t3c=1%m&%ei)nd)6nek!ci++i'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', True)
